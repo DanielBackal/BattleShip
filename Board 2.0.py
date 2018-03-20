cd402326c4a2b4daff7ef9c8cb2357cfbe24f77f
DanielBackal <33614018+DanielBackal@users.noreply.github.com>
2018-03-20 12:02:35 -0400

    Started Guess Function
    r and c will be used to check the hidden board for a ship, hit, or miss and then update said board, as well as board 1 if it was a new hit. If it was already guessed, will allow user to guess again.

--- Board 2.0.py
+++ Board 2.0.py
@@ -15,6 +15,18 @@ a ship, lower S_Total by 1. When it hits zero, that player wins.
 class Board:
 
     def __init__(self):
-        self._hb = [[chr(735) for i in range(10)] for i in range(10)]
-        self._b1 = [[chr(735) for i in range(10)] for i in range(10)]
-        self._b2 = [[chr(735) for i in range(10)] for i in range(10)] 
+        self._hb = [[chr(164) for i in range(10)] for i in range(10)]
+        self._b1 = [[chr(164) for i in range(10)] for i in range(10)]
+        self._b2 = [[chr(164) for i in range(10)] for i in range(10)]
+        self._r = {'A' : 0, 'B' : 1, 'C' : 2, 'D' : 3, 'E' : 4, 'F' : 5, 'G' : 6, 'H' : 7, 'I' : 8, 'J' : 9}
+        self._c = {'1' : 0, '2' : 1, '3' : 2, '4' : 3, '5' : 4, '6' : 5, '7' : 6, '8' : 7, '9' : 8, '10' : 9}
+
+    def guess(self):#player guess function
+        r, c = input("what row/column do you want to guess? (letter followed by number, no spaces)    ")
+        r = r.upper()
+        r = self._r[r]
+        c = self._c[c]
+        print(r)
+        print(c)
+        
+        
